abcefaa9c05b763424628d33dfe863441fe3c1a4
Sana Khalid <sanabibi.khalid@gmail.com>
2021-03-15 22:35:00 -0700

    append function

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const addButton = document.querySelector('.addButton');
-
+const bookSection = document.querySelector('.bookSelection')
 
 let myLibrary = [];
 
@@ -23,6 +23,25 @@ function addBookToLibrary() {
     newBook.read = bookForm.read.value;
 
     myLibrary.push(newBook);
-
 }
+}
+
+function appendBook(array) {
+    for (i = 0; i < array.length; i++) {
+        let newContainer = document.createElement('div')
+        let heading = document.createElement('h2');
+        let newTitle = document.createElement('p');
+        let newAuthor = document.createElement('p');
+        let newGenre = document.createElement('p');
+        let hasRead = document.createElement('p');
+        heading.textContent = `Book ${i + 1}`
+        newTitle.textContent = array[i].title;
+        newAuthor.textContent = array[i].author;
+        newGenre.TextContent = array[i].genre;
+        bookSection.append(newContainer);
+        newContainer.append(heading);
+        newContainer.append(newTitle);
+        newContainer.append(newAuthor);
+        newContainer.append(newGenre);
+    }
 }
